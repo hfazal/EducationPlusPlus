a183e86f608dca09feb0579c6bbe32004d5c4512
hfazal <husain@fazal.ca>
2012-11-23 18:56:25 -0500

    Seperated Test Code [PES_Class_Tester.php] from classes code [epp_classes.php].

--- educationplusplus/epp_classes.php
+++ educationplusplus/epp_classes.php
@@ -103,26 +103,4 @@
 			return $stringToReturn;
 		}
 	}
-	
-	//TESTER AREA TO TEST YOUR CLASSES
-	//Activity 1
-	$act1 = new Activity("Test 1", 20);
-	
-	//Activity 2
-	$act2 = new Activity("Test 2", 20);
-	
-	//Requirement1
-	$req1 = new Requirement($act1, ">", 50);
-	
-	//Requirement2
-	$req2 = new Requirement($act2, ">", 50);
-	
-	//Array of Requirement Objects for a PES
-	$arrayOfRequirements = array($req1, $req2);
-	
-	//PointEarningScenario
-	$pes = new PointEarningScenario("Pass all Tests", 1000, "Pass all tests in this course and earn 1000 points", $arrayOfRequirements, new DateTime("now"), false);
-	
-	// View the PES
-	print $pes;
 ?>
