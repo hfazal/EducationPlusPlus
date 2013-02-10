1bed93eb6fbcd96e0a24ed232744d16adf02006a
hfazal <husain@fazal.ca>
2013-02-10 05:30:32 -0500

    Added Initial School Leaderboard (Now working)

---
+++ educationplusplus/leaderboardSchool.php
@@ -0,0 +1,143 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Prints a particular instance of educationplusplus
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage educationplusplus
+ * @copyright  2013 Husain Fazal, Preshoth Paramalingam, Robert Stancia
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/// (Replace educationplusplus with the name of your module and remove this line)
+
+require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
+require_once(dirname(__FILE__).'/lib.php');
+
+$id = optional_param('id', 0, PARAM_INT); // course_module ID, or
+$n  = optional_param('n', 0, PARAM_INT);  // educationplusplus instance ID - it should be named as the first character of the module
+$added = optional_param('newpes', 0, PARAM_INT);
+
+if ($id) {
+    $cm         = get_coursemodule_from_id('educationplusplus', $id, 0, false, MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
+    $educationplusplus  = $DB->get_record('educationplusplus', array('id' => $cm->instance), '*', MUST_EXIST);
+} elseif ($n) {
+    $educationplusplus  = $DB->get_record('educationplusplus', array('id' => $n), '*', MUST_EXIST);
+    $course     = $DB->get_record('course', array('id' => $educationplusplus->course), '*', MUST_EXIST);
+    $cm         = get_coursemodule_from_instance('educationplusplus', $educationplusplus->id, $course->id, false, MUST_EXIST);
+} else {
+    error('You must specify a course_module ID or an instance ID');
+}
+
+require_login($course, true, $cm);
+$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+
+add_to_log($course->id, 'educationplusplus', 'view', "view.php?id={$cm->id}", $educationplusplus->name, $cm->id);
+
+/// Print the page header
+
+$PAGE->set_url('/mod/educationplusplus/view.php', array('id' => $cm->id));
+$PAGE->set_title(format_string($educationplusplus->name));
+$PAGE->set_heading(format_string($course->fullname));
+$PAGE->set_context($context);
+
+// other things you may want to set - remove if not needed
+//$PAGE->set_cacheable(false);
+//$PAGE->set_focuscontrol('some-html-id');
+//$PAGE->add_body_class('educationplusplus-'.$somevar);
+
+// Output starts here
+echo $OUTPUT->header();
+echo $OUTPUT->heading('Education++: School Leaderboard');
+
+global $DB;
+$studentIds = array();
+$studentIdsUnique = array();
+
+$students = $DB->get_records('epp_student',array('leaderboardoptstatus'=>0));
+
+foreach ($students as $entry){
+	array_push($studentIds, $entry->student_id);
+}
+
+$studentIdsUnique = array_unique($studentIds);
+
+echo '	<script src="sorttable.js"></script>
+		<style>
+			table.sortable thead {
+				background-color:#eee;
+				color:#666666;
+				font-weight: bold;
+				cursor:default;
+			}
+		</style>
+
+		<table class="sortable" style="margin: auto;">
+		<thead>
+			<tr>
+				<td style="cursor:pointer;cursor:hand;">Name</td>
+				<td style="cursor:pointer;cursor:hand;">Badges Accumulated</td>
+			</tr>
+		</thead>
+		<tbody>';
+
+$allIncentives = $DB->get_records('epp_incentive');
+		
+foreach ($studentIdsUnique as $epp_student){
+	$allRecords = $DB->get_records('epp_student',array('student_id'=>$epp_student));
+	
+	foreach ($allRecords as $firstrecord){
+		if ($USER->id == $firstrecord->student_id){
+			echo '<tr style="background-color:#BCED91;">';
+		}
+		else {
+			echo '<tr>';
+		}
+		
+		echo '  <td style="font-weight:bold">' . $firstrecord->firstname . ' ' . $firstrecord->lastname . '</td>
+				<td style="text-align:left;max-width:150px;">';
+
+		$thisStudentsBadges = $DB->get_records('epp_student_badge',array('student_id'=>$firstrecord->id));
+		echo count($thisStudentsBadges) . " "; // displays number of badges earned
+		
+		foreach ($thisStudentsBadges as $earnedBadge){
+			foreach ($allIncentives as $incentive){
+				if ($incentive->id == $earnedBadge->badge_id){
+					echo '<img style="width:25px;height:25px;" src="data:image/jpg;base64,' . $incentive->icon . '" alt="' . $incentive->name . '" />';
+				}
+			}
+		}
+		
+				
+		echo '</td></tr>';
+		break;
+	}
+}
+
+echo '</tbody>
+	  </table>';
+
+echo '<br/><br/><br/>';
+echo $OUTPUT->box('<div style="width:100%;text-align:center;"><a href="view.php?id='. $cm->id .'">Return to the Education++ homepage</a></div>');
+	  
+// Finish the page
+echo $OUTPUT->footer();
